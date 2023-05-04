e378fe829e7709d81ef558757dfdd5700d122e10
jhaskel <johaskel@gmail.com>
2023-05-03 21:00:47 -0300

    mais mega

--- server.js
+++ server.js
@@ -31,13 +31,16 @@ const resgates = require('./routes/resgatesRoutes');
 const { credential } = require('firebase-admin');
 
 
+
+
 const port = normalizePort( process.env.PORT || '3000');
 app.set('port',port);
 const server= http.createServer(app);
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({
-    extended: true
+    extended: true,
+    limit: '50mb'
 }));
 app.use(cors());
 app.use(passport.initialize());
